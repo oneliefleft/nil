a473aecf08f5ea86f5ba646f29408f934158dbb5
oneliefleft <oneliefleft@gmail.com>
2014-08-24 02:12:46 +0000

    Put in a generic tensor base class.

---
+++ tensor_base.h
@@ -0,0 +1,157 @@
+// -------------------------------------------------------------------
+// @author Toby D. Young
+//
+// Copyright 2014 nil authors. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above
+//    copyright notice, this list of conditions and the following
+//    disclaimer in the documentation and/or other materials provided
+//    with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE NAMEPSACE EWALENA AUTHORS ``AS
+// IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// NAMESPACE EWALENA AUTHORS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+// OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// The views and conclusions contained in the software and
+// documentation are those of the authors and should not be
+// interpreted as representing official policies, either expressed or
+// implied, of the namespace ewalena authors.
+// -------------------------------------------------------------------
+
+#ifndef __nil_tensor_base_h
+#define __nil_tensor_base_h
+
+#include <deal.II/base/tensor.h>
+
+#include "group_symmetry.h"
+
+#include <fstream>
+#include <iostream>
+#include <list>
+
+namespace nil
+{
+
+  /**
+   * \brief Tensor Base.
+   *
+   * This is the base class for an \f$N\,\f$-order tensors.
+   *
+   * @note In the inline documentation, Various mappings from Voight
+   * notation to proper tensor notation is taken from:
+   *
+   * J. F. Nye, "Własnosści fizyczne kryształów w Ujęciu tensorowym i
+   * macierzowym" Państwowe Wydawnictwo Naukowa (1962). Tłumaczył z
+   * języka angielskiego J. Rauułuszhiewicz.
+   *
+   * @author Toby D. Young 2014.
+   */  
+  template <enum GroupSymmetry, int rank, typename ValueType = double>
+    class TensorBase
+    :
+    public dealii::Tensor<rank, 3, ValueType>
+    {
+    public:
+    
+
+    /**
+     * Constructor. 
+     */
+    TensorBase ();
+    
+
+    /**
+     * Destructor. 
+     */
+    ~TensorBase (); 
+
+
+    /**
+     * Distribute coefficients on to the tensor.
+     */
+    void distribute_coefficients ();
+    
+
+    /**
+     * Reinitialise (zero out) this tensor.
+     */
+    void reinit ();
+
+
+    /**
+     * Make the order of this tensor public.
+     */
+    unsigned int order () const;
+    
+
+    /**
+     * Make the dimension of this tensor public. @note This function
+     * just returns the integer three, since these tensors are defined
+     * in three-dimensions only.
+     */
+    unsigned int dim () const;
+
+
+    /* /\** */
+    /*  * Make the group symmetry of this tensor public. */
+    /*  *\/ */
+    /* std::string group_symmetry () const; */
+   
+
+    protected:
+    
+
+    /**
+     * Make the order of this tensor known to all derived classes.
+     */
+    const int order_; 
+    
+
+    /**
+     * Make the group symmetry of this tensor known to all derived classes.
+     */
+    GroupSymmetry group_symmetry_; 
+    
+
+    /**
+     * Zero out a tensor. @note Only zero is allowed as an input to this function
+     */
+    /* operator = ValueType; */
+    
+
+    private:
+
+
+    /**
+     * The underlying tensor.
+     */
+    dealii::Tensor<rank, 3, ValueType> tensor; 
+
+    }; /* TensorBase */
+
+  
+  /* ----------------- Non-member functions operating on tensors. ------------ */
+  
+  
+} /* namespace nil */
+
+
+#endif /* __nil_tensor_base_h */
+
+
