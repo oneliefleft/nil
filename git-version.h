5ebdcf6dffc94413dbefdd82e393d6a6816b07b4
oneliefleft <oneliefleft@gmail.com>
2014-09-03 13:10:15 +0200

    Git version from Denis Davydov.

---
+++ git-version.h
@@ -0,0 +1,11 @@
+#ifndef GITVERSION_H
+#define GITVERSION_H
+
+#include <string>
+
+extern const std::string gGIT_VERSION;
+extern const std::string gGIT_LOG;
+extern const std::string gGIT_REFSPEC;
+extern const std::string gGIT_SHA1;
+
+#endif //GITVERSION_H
