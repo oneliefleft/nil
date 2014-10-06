84f89457b2b9e61404b81b8b397c0631c95bb25e
oneliefleft <oneliefleft@gmail.com>
2014-10-06 17:57:25 +0000

    Remove a typo in the license.

--- spontaneous_polarization_tensor.cc
+++ spontaneous_polarization_tensor.cc
@@ -29,7 +29,7 @@
 // The views and conclusions contained in the software and
 // documentation are those of the authors and should not be
 // interpreted as representing official policies, either expressed or
-// implied, of the namespace ewalena authors.
+// implied, of the authors.
 // -------------------------------------------------------------------
 
 #include "spontaneous_polarization_tensor.h"
